dd9c93f8c94ae4a861734206bea65e6a8a6f0535
trantuan <tincanxh@gmail.com>
2018-08-15 10:20:24 +0700

    exercise book singleton

---
+++ src/main/java/bookSingleton/BookBorrower.java
@@ -0,0 +1,30 @@
+package bookSingleton;
+
+public class BookBorrower {
+    private BookSingleton borrowedBook;
+    private boolean haveBook = false;
+
+    public void borrowBook() {
+        borrowedBook = BookSingleton.borrowBook();
+
+        if (borrowedBook == null) {
+            haveBook = false;
+        } else {
+            haveBook = true;
+        }
+    }
+
+    public String getAuthorAndTitle(){
+        System.out.println(haveBook);
+        if(haveBook){
+            System.out.println(haveBook);
+            return borrowedBook.getAuthorAndTitle();
+        }
+        return "I don't have the book";
+    }
+
+    public void returnBook(){
+        borrowedBook.returnBook(borrowedBook);
+    }
+}
+
